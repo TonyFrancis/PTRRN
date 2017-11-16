863f2b877909401828056d009bb05e40b746d309
George Tony <gtony@suyati.com>
2017-11-16 12:11:09 +0530

    changeS

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import RefreshableList from './lib';
+import RefreshableList from './lib';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Text> Hello </Text>
+      <RefreshableList />
     );
   }
 }
